86c2f57fdf29b8195ba3d32bcd62454ddf2b0083
Ben Strong <bcstrong43@gmail.com>
2021-08-05 12:41:50 -0400

    fixed obstacle issues. obstacles are now part of navmesh

---
+++ LuckOfTheDraw/Assets/Scripts/AI Stuff/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.AI_Stuff
+{
+    public class EnemyController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
